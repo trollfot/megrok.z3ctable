107f5048b2ca2d10facf5137bd6315f234d584eb
Christian Klinger <cklinger@novareto.de>
2009-09-15 16:31:06 +0200

    Reflect the changes to the split of megrok.layout

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0.1'
+version = '1.1.0'
 
 setup(name='megrok.z3ctable',
       version=version,
       description="A table component package for zope3 and Grok based on z3c.table.",
-      long_description = open(os.path.join("megrok/z3ctable", "README.txt")
-	                              ).read() + "\n",
+      long_description=open(os.path.join('src/megrok/z3ctable',"README.txt")).read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
           "Framework :: Zope3",
           "Programming Language :: Python",
@@ -21,7 +21,8 @@ setup(name='megrok.z3ctable',
       author_email='cklinger@novareto.de',
       url='http://pypi.python.org/pypi/megrok.z3ctable',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      package_dir={'':'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
       namespace_packages=['megrok'],
       include_package_data=True,
       zip_safe=False,
