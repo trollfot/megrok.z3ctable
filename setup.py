955f9f95206958617bd2fad7ba8d11318bf20707
Souheil CHELFOUH <trollfot@vittra.org>
2009-09-29 14:58:41 +0200

    Updated versions and README for a release

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 import os
 
 version = '1.3.0'
+readme = open(os.path.join("src", "megrok", "z3ctable", "README.txt")).read()
+history = open(os.path.join("docs", "HISTORY.txt")).read()
 
 setup(name='megrok.z3ctable',
       version=version,
-      description="A table component package for zope3 and Grok based on z3c.table.",
-      long_description=open(os.path.join('src/megrok/z3ctable',"README.txt")).read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="A table component package for Grok based on z3c.table.",
+      long_description= u"%s\n\n%s" % (readme, history),
       classifiers=[
           "Framework :: Zope3",
           "Programming Language :: Python",
